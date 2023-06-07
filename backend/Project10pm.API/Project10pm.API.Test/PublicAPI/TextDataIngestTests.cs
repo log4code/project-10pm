9dd55a6b987b2bd9ee0f03f2963867452cfd0904
log4code <jt@jonathantaylor.dev>
2023-06-07 00:00:32 -0400

    Initial solution commit. Simple text data POST endpoint with tests

---
+++ backend/Project10pm.API/Project10pm.API.Test/PublicAPI/TextDataIngestTests.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+using static Project10pm.API.DataIngest.TextController;
+
+namespace Project10pm.API.Test.PublicAPI
+{
+    [TestFixture]
+    internal class TextDataIngestTests
+    {
+        private const string TEXT_POST_ENDPOINT = @"/api/v1/text";
+        private WebApplicationFactory<Program> _appFactory;
+
+        [SetUp]
+        public void SetUp() 
+        {
+            _appFactory = new WebApplicationFactory<Program>();
+        }
+
+        [TestCase("2023-06-06")]
+        public async Task TextPost_NonEmptyContent_Returns200(string input)
+        {
+            var model = new NewText()
+            {
+                Text = input
+            };
+            var client = _appFactory.CreateClient();
+            var response = await client.PostAsync(TEXT_POST_ENDPOINT, JsonContent.Create(model));
+            Assert.That(((int)response.StatusCode), Is.EqualTo(StatusCodes.Status200OK));
+        }
+
+        [TestCase("2023-06-06")]
+        public async Task TextPost_NonEmptyContent_ReturnsInputText(string input)
+        {
+            var model = new NewText()
+            {
+                Text = input
+            };
+            var client = _appFactory.CreateClient();
+            var response = await client.PostAsync(TEXT_POST_ENDPOINT, JsonContent.Create(model));
+            var parseResult = await response.Content.ReadFromJsonAsync<NewTextParseResult>();
+            Assert.That(parseResult?.InputText, Is.EqualTo(input));
+        }
+
+        [Test]
+        public async Task TextPost_EmptyContent_Returns400()
+        {
+            var client = _appFactory.CreateClient();
+            var response = await client.PostAsync(TEXT_POST_ENDPOINT, JsonContent.Create(string.Empty));
+            Assert.That(((int)response.StatusCode), Is.EqualTo(StatusCodes.Status400BadRequest));
+        }
+
+        [Test]
+        public async Task TextPost_NoContent_Returns400()
+        {
+            var client = _appFactory.CreateClient();
+            var response = await client.PostAsync(TEXT_POST_ENDPOINT, JsonContent.Create((object?)null));
+            Assert.That(((int)response.StatusCode), Is.EqualTo(StatusCodes.Status400BadRequest));
+        }
+    }
+}
---
+++ backend/Project10pm.API/Project10pm.API/DataIngest/TextController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace Project10pm.API.DataIngest
+{
+    [ApiController]
+    [Route("api/v1/text")]
+    public class TextController : Controller
+    {
+        /// <summary>
+        /// Add new text based content for parsing
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(NewTextParseResult))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status415UnsupportedMediaType)]
+        public IActionResult Post([FromBody] NewText model)
+        {
+            var result = new NewTextParseResult()
+            {
+                InputText = model.Text
+            };
+            return Ok(result);
+        }
+
+        public class NewText
+        {
+            [Required]
+            public string Text { get; set; } = string.Empty;
+        }
+
+        public class NewTextParseResult
+        {
+            public string? InputText { get; set; }
+        }
+    }
+}
