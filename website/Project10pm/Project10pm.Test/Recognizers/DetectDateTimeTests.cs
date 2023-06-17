ce81d30fd2641e0e45339bb9f4adf7e6a062c8ef
log4code <jt@jonathantaylor.dev>
2023-06-17 16:29:28 -0400

    New date/time recognizer based on Microsoft text recognizers

---
+++ website/Project10pm/Project10pm.Test/Recognizers/DetectDateTimeTests.cs
@@ -0,0 +1,54 @@
+﻿using Newtonsoft.Json;
+using Project10pm.Recognizers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project10pm.Test.Recognizers
+{
+    [TestFixture]
+    internal class DetectDateTimeTests
+    {
+        [TestCase(DateTimeRecognition.ENGLISH_CULTURE, "06-14-2023", "06-14-2023")]
+        [TestCase(DateTimeRecognition.ENGLISH_CULTURE, "06/14/2023", "06/14/2023")]
+        [TestCase(DateTimeRecognition.ENGLISH_CULTURE, "2023-06-14", "2023-06-14")]
+        public void SingleExplicitInstance_FullDate_ReturnsValidResult(string culture, string content, string dateText)
+        {
+            var results = DateTimeRecognition.DetectDateTimeReferences(content, culture);
+            var expectedResult = results.First(i => i.SnippetText == dateText);
+
+            Assert.That(results.Count, Is.EqualTo(1));
+            Assert.That(expectedResult.SnippetStartIndex, Is.GreaterThanOrEqualTo(0));
+            Assert.That(expectedResult.SnippetEndIndex, Is.GreaterThanOrEqualTo(0));
+            Assert.That(expectedResult.RecognitionTypeName, Is.Not.Null);
+            Assert.That(expectedResult.SnippetText, Is.Not.Null);
+        }
+
+        [TestCase(DateTimeRecognition.ENGLISH_CULTURE, "06-14-2023 and 06-15-2023", 2)]
+        public void MultipleExplicitInstance_FullDate_ReturnsValidResults(string culture, string content, int count)
+        {
+            var results = DateTimeRecognition.DetectDateTimeReferences(content, culture);
+
+            Assert.That(results.Count, Is.EqualTo(2));
+        }
+
+        [TestCase(DateTimeRecognition.ENGLISH_CULTURE, "today", "today")]
+        [TestCase(DateTimeRecognition.ENGLISH_CULTURE, "tomorrow", "tomorrow")]
+        [TestCase(DateTimeRecognition.ENGLISH_CULTURE, "yesterday", "yesterday")]
+        public void ImpliedInstance_FullDate_ReturnsValidResult(string culture, string content, string dateText)
+        {
+            var results = DateTimeRecognition.DetectDateTimeReferences(content, culture);
+            var expectedResult = results.First(i => i.SnippetText == dateText);
+
+            Assert.That(results.Count, Is.EqualTo(1));
+            Assert.That(expectedResult.SnippetStartIndex, Is.GreaterThanOrEqualTo(0));
+            Assert.That(expectedResult.SnippetEndIndex, Is.GreaterThanOrEqualTo(0));
+            Assert.That(expectedResult.RecognitionTypeName, Is.Not.Null);
+            Assert.That(expectedResult.SnippetText, Is.Not.Null);
+        }
+
+
+    }
+}
---
+++ website/Project10pm/Project10pm/Recognizers/DateTimeRecognition.cs
@@ -0,0 +1,55 @@
+﻿
+
+using Microsoft.Recognizers.Text;
+using Microsoft.Recognizers.Text.DateTime;
+using Newtonsoft.Json;
+using static Project10pm.Controllers.ContentController;
+
+namespace Project10pm.Recognizers
+{
+    public static class DateTimeRecognition
+    {
+        public const string DEFAULT_CULTURE = Culture.English;
+        public const string ENGLISH_CULTURE = Culture.English;
+
+        public static List<DateTimeRecognitionResult> DetectDateTimeReferences(string content, string culture = DEFAULT_CULTURE)
+        {
+            List<ModelResult> rawResults =
+                    DateTimeRecognizer.RecognizeDateTime(content, culture);
+
+            var results = new List<DateTimeRecognitionResult>(rawResults.Count);
+            foreach (var rawResult in rawResults)
+            {
+                results.Add(new DateTimeRecognitionResult
+                {
+                    SnippetStartIndex = rawResult.Start,
+                    SnippetEndIndex = rawResult.End,
+                    SnippetText = rawResult.Text,
+                    RecognitionTypeName = rawResult.TypeName,
+                    Resolution = rawResult.Resolution,
+                });
+
+                //TODO: Temporary to learn the structure of these values
+                Console.WriteLine(JsonConvert.SerializeObject(rawResult.Resolution, Formatting.Indented));
+            }
+
+            return results;
+        }
+    }
+
+    public class DateTimeRecognitionResult
+    {
+        public int SnippetStartIndex { get; set; } = -1;
+        public int SnippetEndIndex { get; set;} = -1;
+        public string SnippetText { get; set; } = string.Empty;
+        public string RecognitionTypeName { get; set; } = string.Empty;
+
+        //TODO: Don't know yet what to do with this because there isn't much documentation. Will take a bit of time to discover patterns in how to use
+        public SortedDictionary<string, object> Resolution { get; set; } = new SortedDictionary<string, object>();
+    }
+
+    public class DateTimeRecognitionParts
+    {
+
+    }
+}
